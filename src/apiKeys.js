9f03712dcb3dcce732ff24998372fe450ce14d5a
Subhranshu Patra <subhranshupatra7@gmail.com>
2023-08-19 12:05:25 +0530

    initial commit

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "af2374845a50c99314015c9071a78514",
   base: "https://api.openweathermap.org/data/2.5/",
 };
